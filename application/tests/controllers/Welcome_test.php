bcf234bc1d0cc05cbd1647f55732770ee853ce55
kenjis <kenji.uui@gmail.com>
2016-01-08 09:53:34 +0900

    Fix Welcome_test

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -12,24 +12,13 @@ class Welcome_test extends TestCase
 {
 	public function test_index()
 	{
-		$output = $this->request('GET', ['Welcome', 'index']);
+		$output = $this->request('GET', 'welcome');
 		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
 	}
 
 	public function test_method_404()
 	{
-		$this->request('GET', ['Welcome', 'method_not_exist']);
+		$this->request('GET', 'welcome/method_not_exist');
 		$this->assertResponseCode(404);
-	}
-
-	public function test_APPPATH()
-	{
-		$actual = realpath(APPPATH);
-		$expected = realpath(__DIR__ . '/../..');
-		$this->assertEquals(
-			$expected,
-			$actual,
-			'Your APPPATH seems to be wrong. Check your $application_folder in tests/Bootstrap.php'
-		);
 	}
 }
