2c8b0f48c154b4c67d7b1ec331840fc36b7e754c
kenjis <kenji.uui@gmail.com>
2016-01-08 10:07:31 +0900

    Fix test class name

--- application/tests/modules/welcome/controllers/Welcome_test.php
+++ application/tests/modules/welcome/controllers/Welcome_test.php
@@ -8,7 +8,7 @@
  * @link       https://github.com/kenjis/ci-phpunit-test
  */
 
-class Welcome_test extends TestCase
+class welcome_Welcome_test extends TestCase
 {
 	public function test_index()
 	{
